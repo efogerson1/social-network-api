250739ed6e7d2dc52246b6c05f1c327527f886c7
efogerson1 <efogerson2@gmail.com>
2023-09-06 15:14:54 -0700

    added Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,48 @@
-const { Schema, model} = require('mongoose')
+const { Schema, model} = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                return new Date(date).toLocaleString()
+            }
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+
+        reactions: [Reaction]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+
+        id: false
+    }
+)
+
+// Adding reactionCount by returning reactions list length
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function (){
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema)
+
+module.exports = Thought;

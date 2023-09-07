a8d2bf47561c4fabe72d87d998233df3d23fdfe5
efogerson1 <efogerson2@gmail.com>
2023-09-07 11:45:26 -0700

    seeds

--- utils/data.js
+++ utils/data.js
@@ -1,7 +1,7 @@
 const users = [
     {
-        username: 'Abenezer',
-        email: 'abenezer@gmail.com',
+        username: 'John',
+        email: 'john@gmail.com',
         friends: [
             'berny',
             'leah'
@@ -15,8 +15,8 @@ const users = [
         thoughts: []
     },
     {
-        username: 'Cathy',
-        email: 'cathy@gmail.com',
+        username: 'Eric',
+        email: 'eric@gmail.com',
         friends: [],
         thoughts: []
     },

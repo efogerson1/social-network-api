85f6f80a756e191a3eafc0099437f34cd5318a43
efogerson1 <efogerson2@gmail.com>
2023-09-08 09:45:13 -0700

    routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,10 @@
-const { getThoughts, createThought, getSingleThought, updateThought, deleteThought, createReaction, removeReaction } = require('../../controllers/thoughtController');
+const { getThoughts, createThought, getThoughtById, updateThought, deleteThought, createReaction, removeReaction } = require('../../controllers/thoughtController');
 
 const router = require('express').Router();
 
 router.route('/').get(getThoughts).post(createThought);
 
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
 
 router.route('/:thoughtId/reactions').post(createReaction).delete(removeReaction);
 
